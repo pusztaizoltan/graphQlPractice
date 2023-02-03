3ad164c0a204e483fc763c68d035393160e2ebd0
pusztaizoltan <a.pusztaizoltan@gmail.com>
2023-02-03 16:16:00 +0100

    clear up redundant methods from Schemable.java

--- src/main/java/org/example/entity/Schemable.java
+++ src/main/java/org/example/entity/Schemable.java
@@ -1,29 +1,18 @@
 package org.example.entity;
 
-import graphql.Scalars;
-import graphql.schema.GraphQLFieldDefinition;
-import graphql.schema.GraphQLObjectType;
-import graphql.schema.GraphQLScalarType;
 import graphql.schema.idl.TypeRuntimeWiring;
 
 import java.lang.reflect.Field;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public interface Schemable {
-
-    default void experimentMethod(){
-
+    default void experimentMethod() {
         System.out.println(this.getClass().getSimpleName());
     }
-
-
 
     static TypeRuntimeWiring TypeRuntimeWiringFromClass(Class<?> classType) {
         TypeRuntimeWiring.Builder builder = new TypeRuntimeWiring.Builder().typeName(classType.getSimpleName());
         Field[] fields = classType.getDeclaredFields();
-        for (Field field: fields) {
+        for (Field field : fields) {
             Class<?> fieldType = field.getType();
             String fieldName = field.getType().getSimpleName();
             builder = builder.dataFetcher(fieldName, env ->
@@ -31,28 +20,4 @@ public interface Schemable {
         }
         return builder.build();
     }
-
-
-    static GraphQLObjectType graphQLObjectTypeFromClass(Class<?> classType){
-        GraphQLObjectType.Builder typeBuilder = GraphQLObjectType.newObject().name(classType.getSimpleName());
-        Field[] fields = classType.getDeclaredFields();
-        for (Field field: fields) {
-            GraphQLScalarType fieldType = typeAdapter(field.getType().getSimpleName());
-            typeBuilder = typeBuilder.field(GraphQLFieldDefinition.newFieldDefinition()
-                                                                  .name(field.getName())
-                                                                  .type(fieldType));
-        }
-        return typeBuilder.build();
-    }
-
-    private static GraphQLScalarType typeAdapter(String javaType){
-        switch (javaType) {
-            case "long" -> {return Scalars.GraphQLInt;}
-            case "String" -> {return Scalars.GraphQLString;}
-            // ...
-        }
-        return null;
-    }
-
-
 }
