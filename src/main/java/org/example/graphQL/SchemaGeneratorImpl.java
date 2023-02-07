7f32bac50f3c6716e74fd8195c7dc187e9673a31
pusztaizoltan <a.pusztaizoltan@gmail.com>
2023-02-08 00:53:57 +0100

    add javadoc to SchemaGeneratorImpl public methods

--- src/main/java/org/example/graphQL/SchemaGeneratorImpl.java
+++ src/main/java/org/example/graphQL/SchemaGeneratorImpl.java
@@ -8,15 +8,24 @@ public class SchemaGeneratorImpl {
     private final ClassParser classParser = new ClassParser();
     private final GraphQLBuilder builder = new GraphQLBuilder();
 
+    /**
+     * Constructor for SchemaGeneratorImpl
+     * with required dataSource instance as argument
+     */
     public SchemaGeneratorImpl(Object dataService) {
         this.classParser.parseClassesFromDataService(dataService);
         this.builder.addQueryForDataService(dataService);
     }
-
+    /**
+     * Method to provide optional Type patterns for SchemaBuilding
+     */
     public void withAdditionalClasses(Class<?>... classes) {
         this.classParser.parseAdditionalClasses(classes);
     }
 
+    /**
+     * Build method of SchemaGeneratorImpl
+     */
     public GraphQL getGraphQL() {
         builder.addTypesForComponentClasses(this.classParser.getComponents());
         return GraphQL.newGraphQL(builder.build()).build();
