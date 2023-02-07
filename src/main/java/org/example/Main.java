0050d636fb3608555e53f8415f498cda9a68bceb
pusztaizoltan <a.pusztaizoltan@gmail.com>
2023-02-07 23:02:44 +0100

    clean um main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,32 +2,22 @@ package org.example;
 
 import graphql.ExecutionResult;
 import graphql.GraphQL;
-import graphql.schema.GraphQLObjectType;
-import graphql.schema.TypeResolver;
 import org.example.db.ListDbTestImpl;
-import org.example.entity.Author;
-import org.example.entity.Book;
-import org.example.entity.Reader;
-import org.example.entity.TestClass;
 import org.example.graphQL.SchemaGeneratorImpl;
-
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 
 public class Main {
     public static void main(String[] args) {
-        task();
+        testTasks();
     }
 
-    static void task() {
+    static void testTasks() {
         SchemaGeneratorImpl generator = new SchemaGeneratorImpl(new ListDbTestImpl());
         GraphQL build = generator.getGraphQL();
-//        graphQLEnumTypeFromEnum(GenreType.class);
         System.out.println("-------------------------ALL FROM TESTCLASSES----------");
         ExecutionResult er2 = build.execute("{allTestClass {id, content}}");
         er2.getErrors().forEach(System.out::println);
         System.out.println(build.execute("{allTestClass {id, content}}").getData().toString());
-        System.out.println("-------------------------TESTcLASS BY ID-----------");
+        System.out.println("-------------------------TESTCLASS BY ID-----------");
         ExecutionResult er1 = build.execute("{testClassById(id: 1){id, content}}");
         er1.getErrors().forEach(System.out::println);
         System.out.println(build.execute("{testClassById(id: 1){id, content}}").getData().toString());
@@ -47,42 +37,5 @@ public class Main {
         ExecutionResult er6 = build.execute("{bookById(id: 2) {id, title, genreAsEnum, author{name,isAlive}}}");
         er6.getErrors().forEach(System.out::println);
         System.out.println(build.execute("{bookById(id: 2) {id, title, genreAsEnum, author{name,isAlive}}}").getData().toString());
-    }
-
-    static void methodReflectionExperiments(Object datasource) {
-        Method[] methods = datasource.getClass().getDeclaredMethods();
-        for (Method method : methods) {
-            if (Modifier.isPublic(method.getModifiers())) {
-                if (method.getParameters().length == 1) {
-                    System.out.println("" + method.getName());
-//                    System.out.println("" + method.getParameters()[0].getAnnotations().length >=1);
-////                    System.out.println(method);
-//                    var aa = ((Class<?>) ((ParameterizedType) method.getGenericReturnType()).getActualTypeArguments()[0]).getSimpleName();
-//                    System.out.println("" + method.getName() + " " + ((ParameterizedType) method.getGenericReturnType()).getActualTypeArguments()[0]);
-//                    System.out.println("- " + aa);
-//                    System.out.println("- " + method.getReturnType().getSimpleName());
-                    //                    System.out.println("- "+method.getModifiers());
-//                    System.out.println("- "+(method.getAnnotatedReturnType().getType().getTypeName()));
-//                String genericTypeName = ((ParameterizedType)field.getGenericType()).getActualTypeArguments()[0].getClass().getSimpleName();
-                }
-            }
-        }
-    }
-
-    static TypeResolver getTypeResolver() {
-        return (env) -> {
-            GraphQLObjectType result = null;
-            Object obj = env.getObject();
-            if (obj instanceof TestClass) {
-                result = env.getSchema().getObjectType("TestClass");
-            } else if (obj instanceof Book) {
-                result = env.getSchema().getObjectType("Book");
-            } else if (obj instanceof Author) {
-                result = env.getSchema().getObjectType("Author");
-            } else if (obj instanceof Reader) {
-                result = env.getSchema().getObjectType("Reader");
-            }
-            return result;
-        };
     }
 }
