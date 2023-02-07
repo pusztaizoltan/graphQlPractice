495ecbc584a17374804fd9266fa753f42562a848
pusztaizoltan <a.pusztaizoltan@gmail.com>
2023-02-07 21:04:00 +0100

    refactor GraphQLBuilder

--- src/main/java/org/example/graphQL/generatorUtil/GraphQLBuilder.java
+++ src/main/java/org/example/graphQL/generatorUtil/GraphQLBuilder.java
@@ -4,12 +4,11 @@ import graphql.schema.DataFetcher;
 import graphql.schema.FieldCoordinates;
 import graphql.schema.GraphQLCodeRegistry;
 import graphql.schema.GraphQLEnumType;
-import graphql.schema.GraphQLFieldDefinition;
 import graphql.schema.GraphQLObjectType;
 import graphql.schema.GraphQLSchema;
+import org.example.graphQL.annotation.ArgWith;
 import org.example.graphQL.annotation.FieldOf;
 import org.example.graphQL.annotation.FieldType;
-import org.example.graphQL.annotation.ArgWith;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -26,30 +25,37 @@ public class GraphQLBuilder {
 
     public void addQueryForDataService(Object dataService) {
         GraphQLObjectType.Builder queryType = GraphQLObjectType.newObject().name("Query");
-        Method[] methods = dataService.getClass().getDeclaredMethods();
-        for (Method method : methods) {
-            if (!Modifier.isPublic(method.getModifiers()) || !method.isAnnotationPresent(FieldOf.class)) {
-                continue;
-            }
-            FieldType category = method.getAnnotation(FieldOf.class).type();
-            if (method.getParameters().length == 0 && category == FieldType.LIST) {
-                queryType.field(FieldAdapter.listReturnWithoutArg(method));
-                DataFetcher<?> fetcher = (env) -> method.invoke(dataService);
-                registry.dataFetcher(FieldCoordinates.coordinates("Query", method.getName()), fetcher);
-            } else if (method.getParameters().length == 1 &&
-                       category == FieldType.OBJECT &&
-                       method.getParameters()[0].isAnnotationPresent(ArgWith.class)) {
-                // todo rewrite this hatchetJob in possession of
-                GraphQLFieldDefinition field = FieldAdapter.objectReturnByOneArg(method); //todo
-                ArgWith marker = method.getParameters()[0].getAnnotation(ArgWith.class);
-                queryType.field(field);
-                DataFetcher<?> fetcher = (env) -> method.invoke(dataService, env.getArguments().get(marker.name()));
-                registry.dataFetcher(FieldCoordinates.coordinates("Query", field.getName()), fetcher);
-            } else {
-                throw new UnsupportedOperationException("Not implemented yet");
+        for (Method method : dataService.getClass().getDeclaredMethods()) {
+            if (isQueryField(method)) {
+                if (hasListReturnWithoutArg(method)) {
+                    queryType.field(FieldAdapter.listReturnWithoutArg(method));
+                    DataFetcher<?> fetcher = (env) -> method.invoke(dataService);
+                    registry.dataFetcher(FieldCoordinates.coordinates("Query", method.getName()), fetcher);
+                } else if (hasObjectReturnByOneArg(method)) {
+                    queryType.field(FieldAdapter.objectReturnByOneArg(method));
+                    String argName = method.getParameters()[0].getAnnotation(ArgWith.class).name();
+                    DataFetcher<?> fetcher = (env) -> method.invoke(dataService, env.getArguments().get(argName));
+                    registry.dataFetcher(FieldCoordinates.coordinates("Query", method.getName()), fetcher);
+                } else {
+                    throw new RuntimeException("Not implemented type of Query field");
+                }
             }
         }
         graphQLSchema.query(queryType);
+    }
+
+    private boolean isQueryField(Method method) {
+        return Modifier.isPublic(method.getModifiers()) && method.isAnnotationPresent(FieldOf.class);
+    }
+
+    private boolean hasListReturnWithoutArg(Method method) {
+        return method.getParameters().length == 0 && method.getAnnotation(FieldOf.class).type() == FieldType.LIST;
+    }
+
+    private boolean hasObjectReturnByOneArg(Method method) {
+        return method.getParameters().length == 1 &&
+               method.getAnnotation(FieldOf.class).type() == FieldType.OBJECT &&
+               method.getParameters()[0].isAnnotationPresent(ArgWith.class);
     }
 
     public void addTypesForComponentClasses(HashSet<Class<?>> components) {
@@ -70,8 +76,7 @@ public class GraphQLBuilder {
     private void addObjectType(Class<?> component) {
         GraphQLObjectType objectType = TypeAdapter.graphQLObjectTypeFromClass(component);
         graphQLSchema.additionalType(objectType);
-        Field[] fields = component.getDeclaredFields();
-        for (Field field : fields) {
+        for (Field field : component.getDeclaredFields()) {
             if (field.isAnnotationPresent(FieldOf.class)) {
                 Class<?> fieldType = field.getType();
                 String fieldName = field.getType().getSimpleName();
