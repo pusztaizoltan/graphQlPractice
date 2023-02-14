3206b7b4244da428eb3a95418325e3851e4cc5c9
pusztaizoltan <a.pusztaizoltan@gmail.com>
2023-02-14 10:56:21 +0100

    rewrite all sout testTask to junit test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,69 +2,21 @@ package org.example;
 
 import graphql.ExecutionResult;
 import graphql.GraphQL;
-import org.example.db.ListDbTestImpl;
 import org.example.graphql.SchemaGeneratorImpl;
 
 public class Main {
-    public static GraphQL build = new SchemaGeneratorImpl(new ListDbTestImpl()).getGraphQL();
     public static void main(String[] args) {
 //        testQueryTasks();
         testMutationTasks();
     }
 
     static void testMutationTasks() {
-        System.out.println("-------------------------Add newReader----------");
-        ExecutionResult er1 = build.execute("mutation {newReader(readerDTO: {id: 200, fullName:\"fullName_0\", email:\"email_0\"})}");
-        er1.getErrors().forEach(System.out::println);
-        System.out.println(er1.getData().toString());
-        System.out.println();
+//        System.out.println("-------------------------Add newReader----------");
+//        ExecutionResult er1 = build.execute("mutation {newReader(readerDTO: {id: 200, fullName:\"fullName_0\", email:\"email_0\"})}");
+//        er1.getErrors().forEach(System.out::println);
+//        System.out.println(er1.getData().toString());
+//        System.out.println();
 
 
-    }
-    static void testQueryTasks() {
-        System.out.println("-------------------------ALL FROM TEST-CLASSES----------");
-        ExecutionResult er2 = build.execute("{allTestClass {id, content}}");
-        er2.getErrors().forEach(System.out::println);
-        System.out.println(er2.getData().toString());
-        System.out.println();
-        System.out.println("-------------------------TEST-CLASS BY ID-----------");
-        ExecutionResult er1 = build.execute("{testClassById(id: 1){id, content}}");
-        er1.getErrors().forEach(System.out::println);
-        System.out.println(er1.getData().toString());
-        System.out.println();
-        System.out.println("-------------------------ALL FROM Readers----------");
-        ExecutionResult er3 = build.execute("{allReader {id, fullName, email}}");
-        er3.getErrors().forEach(System.out::println);
-        System.out.println(er3.getData().toString());
-        System.out.println();
-        System.out.println("-------------------------Reader BY ID----------");
-        ExecutionResult er4 = build.execute("{readerById(id: 1) {id, fullName, email}}");
-        er4.getErrors().forEach(System.out::println);
-        System.out.println(er4.getData().toString());
-        System.out.println();
-        System.out.println("-------------------------ALL FROM Books----------");
-        ExecutionResult er5 = build.execute("{allBook {id, title, genre}}");
-        er5.getErrors().forEach(System.out::println);
-        System.out.println(er5.getData().toString());
-        System.out.println();
-        System.out.println("-------------------------Books BY ID----------");
-        ExecutionResult er6 = build.execute("{bookById(id: 2) {id, title, genre, author{name}}}");
-        er6.getErrors().forEach(System.out::println);
-        System.out.println(er6.getData().toString());
-        System.out.println();
-        System.out.println("-------------------------Author BY isAlive----------");
-        ExecutionResult er7 = build.execute("{authorByIsAlive(isAlive: true) {name, isAlive}}");
-        er7.getErrors().forEach(System.out::println);
-        System.out.println(er7.getData().toString());
-        System.out.println();
-        System.out.println("-------------------------Books BY titleContent----------");
-        ExecutionResult er8 = build.execute("{bookByTitleContent (titleContent: \"Title_2\") {id, title}}");
-        er8.getErrors().forEach(System.out::println);
-        System.out.println(er8.getData().toString());
-        System.out.println();
-        System.out.println("-------------------------Books BY GenreEnum----------");
-        ExecutionResult er9 = build.execute("{bookByGenre(genre: FICTION) {title, genre}}");
-        er9.getErrors().forEach(System.out::println);
-        System.out.println(er9.getData().toString());
     }
 }
--- src/test/java/org/example/QueryTest.java
+++ src/test/java/org/example/QueryTest.java
@@ -2,13 +2,15 @@ package org.example;
 
 import graphql.ExecutionResult;
 import graphql.GraphQL;
-import org.example.db.ListDbTestImpl;
 import org.example.graphql.SchemaGeneratorImpl;
-import org.junit.jupiter.api.Assertions;
+import org.example.test_db.ListDbTestImpl;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class QueryTest {
     public static GraphQL build = new SchemaGeneratorImpl(new ListDbTestImpl()).getGraphQL();
@@ -17,8 +19,80 @@ class QueryTest {
     void queryAllTestClass_ShouldReturnAllTenTestObject() {
         ExecutionResult result = build.execute("{allTestClass {id, content}}");
         result.getErrors().forEach(System.out::println);
-        Assertions.assertAll(
-                ()->Assertions.assertEquals(0, result.getErrors().size()),
-                ()->Assertions.assertEquals(10, ((List<?>)(((Map<?, ?>) result.getData()).get("allTestClass"))).size()));
+        assertAll(
+                () -> assertEquals(0, result.getErrors().size()),
+                () -> assertEquals(10, ((List<?>) (((Map<?, ?>) result.getData()).get("allTestClass"))).size()));
+    }
+
+    @Test
+    void queryAllReader_ShouldReturnAllTwentyReader() {
+        ExecutionResult result = build.execute("{allReader {id, fullName, email}}");
+        result.getErrors().forEach(System.out::println);
+        assertAll(
+                () -> assertEquals(0, result.getErrors().size()),
+                () -> assertEquals(20, ((List<?>) (((Map<?, ?>) result.getData()).get("allReader"))).size()));
+    }
+
+    @Test
+    void queryAllBook_ShouldReturnAllHundredBook() {
+        ExecutionResult result = build.execute("{allBook {id, title, genre}}");
+        result.getErrors().forEach(System.out::println);
+        assertAll(
+                () -> assertEquals(0, result.getErrors().size()),
+                () -> assertEquals(100, ((List<?>) (((Map<?, ?>) result.getData()).get("allBook"))).size()));
+    }
+
+    @Test
+    void queryTestClassById_ShouldReturnSpecifiedNumberOfFieldValuesWhenExist() {
+        ExecutionResult result = build.execute("{testClassById(id: 1){id, content}}");
+        result.getErrors().forEach(System.out::println);
+        assertAll(
+                () -> assertEquals(0, result.getErrors().size()),
+                () -> assertEquals(2, ((Map<?, ?>) ((Map<?, ?>) result.getData()).get("testClassById")).size()));
+    }
+
+    @Test
+    void queryReaderById_ShouldReturnSpecifiedNumberOfFieldValuesWhenExist() {
+        ExecutionResult result = build.execute("{readerById(id: 1) {id, fullName, email}}");
+        result.getErrors().forEach(System.out::println);
+        assertAll(
+                () -> assertEquals(0, result.getErrors().size()),
+                () -> assertEquals(3, ((Map<?, ?>) ((Map<?, ?>) result.getData()).get("readerById")).size()));
+    }
+
+    @Test
+    void queryBookById_ShouldReturnSpecifiedNumberOfFieldValuesWhenExist() {
+        ExecutionResult result = build.execute("{bookById(id: 2) {id, title, genre, author{name}}}");
+        result.getErrors().forEach(System.out::println);
+        assertAll(
+                () -> assertEquals(0, result.getErrors().size()),
+                () -> assertEquals(4, ((Map<?, ?>) ((Map<?, ?>) result.getData()).get("bookById")).size()));
+    }
+
+    @Test
+    void queryAuthorByIsAlive_ShouldReturnFiveAuthor() {
+        ExecutionResult result = build.execute("{authorByIsAlive(isAlive: true) {name, isAlive}}");
+        result.getErrors().forEach(System.out::println);
+        assertAll(
+                () -> assertEquals(0, result.getErrors().size()),
+                () -> assertEquals(5, ((List<?>) ((Map<?, ?>) result.getData()).get("authorByIsAlive")).size()));
+    }
+
+    @Test
+    void queryByTitleContent_ShouldReturnTenBook() {
+        ExecutionResult result = build.execute("{bookByTitleContent (titleContent: \"Title_2\") {id, title}}");
+        result.getErrors().forEach(System.out::println);
+        assertAll(
+                () -> assertEquals(0, result.getErrors().size()),
+                () -> assertEquals(11, ((List<?>) ((Map<?, ?>) result.getData()).get("bookByTitleContent")).size()));
+    }
+
+    @Test
+    void queryBookByGenreEnum_ShouldReturnTwentyFiveBook() {
+        ExecutionResult result = build.execute("{bookByGenre(genre: FICTION) {title, genre}}");
+        result.getErrors().forEach(System.out::println);
+        assertAll(
+                () -> assertEquals(0, result.getErrors().size()),
+                () -> assertEquals(25, ((List<?>) ((Map<?, ?>) result.getData()).get("bookByGenre")).size()));
     }
 }
