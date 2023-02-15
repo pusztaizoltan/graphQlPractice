7c06bfc49d09aeec6215f701f12f370ace23b687
pusztaizoltan <a.pusztaizoltan@gmail.com>
2023-02-15 12:24:16 +0100

    finish annotation package

--- src/main/java/org/example/graphql/annotation/GQLInput.java
+++ src/main/java/org/example/graphql/annotation/GQLInput.java
@@ -10,6 +10,12 @@ import java.lang.annotation.Target;
  * to be handled as a GraphQL input type, in contrast
  * of default process what is handling objects as
  * output types.
+ * <p>
+ * Classes marked with this annotation are preferably
+ * expected to have a fromMap named method to wire it to its
+ * respective GraphQl input, but it's not strictly necessary,
+ * since during wiring the use of it will be the first option
+ * to try and not the only.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
