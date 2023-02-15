7e42ff8aaa473045460da0b4c1af95e6104a6c9b
pusztaizoltan <a.pusztaizoltan@gmail.com>
2023-02-15 10:55:00 +0100

    nonNull GQLType enum

--- src/main/java/org/example/graphql/annotation/GQLType.java
+++ src/main/java/org/example/graphql/annotation/GQLType.java
@@ -3,6 +3,7 @@ package org.example.graphql.annotation;
 import graphql.Scalars;
 import graphql.schema.GraphQLScalarType;
 
+import javax.annotation.Nonnull;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
@@ -30,25 +31,27 @@ public enum GQLType {
     /**
      * ShortCut method to access the GQLType of a method annotation
      */
-    public static GQLType ofMethod(Method method) {
+    public static GQLType ofMethod(@Nonnull Method method) {
         if (method.isAnnotationPresent(GQLQuery.class)) {
             return method.getAnnotation(GQLQuery.class).type();
-        } else {
+        } else if (method.isAnnotationPresent(GQLMutation.class)) {
             return method.getAnnotation(GQLMutation.class).type();
+        } else {
+            throw new RuntimeException("Expected method annotation is missing on" + method);
         }
     }
 
     /**
      * ShortCut method to access the GQLType of a parameter annotation
      */
-    public static GQLType ofParameter(Parameter parameter) {
+    public static GQLType ofParameter(@Nonnull Parameter parameter) {
         return parameter.getAnnotation(GQLArg.class).type();
     }
 
     /**
      * ShortCut method to access the GQLType of a field annotation
      */
-    public static GQLType ofField(Field field) {
+    public static GQLType ofField(@Nonnull Field field) {
         return field.getAnnotation(GQLField.class).type();
     }
 
